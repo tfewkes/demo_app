e93a37bd6d326fb0001ea0bb1b10e1f4e748e12f
tim <tim@everops.com>
2013-05-07 00:00:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '3b09d3aa003d27692d00fc7a871ba23edbadfd30cdea83e58b577b21e5ee2e3513b051dc4b61026b28d544eddab49ba84b2318850be1893e070b62d7f6e5a5f0'
